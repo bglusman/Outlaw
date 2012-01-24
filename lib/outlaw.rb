c33492e828eb4fcfb3be09ab5302fb897b76fa2e
Shane Emmons <semmons99@gmail.com>
2012-01-24 11:36:05 -0500

    We should move our data/assets into their own folders to keep the toplevel folder as clean as possible. It also gives us a single common directory to find our assets.

--- lib/outlaw.rb
+++ lib/outlaw.rb
@@ -12,11 +12,9 @@ module Outlaw
   def self.enforce(dir=".")
     Outlaw::Enforcement.process_directory(dir)
   end
-  PARAM_TYPES         = [:on_const, :on_ident, :on_ivar, :on_cvar]
-  IGNORE_TYPES        = [:on_sp, :on_nl, :on_ignored_nl, :on_rparen, :on_lparen]
-  SPECIAL_CASES       = [:disjoint_code_seperator] #need to work on naming here
-  CORE_CLASS          = []
-  File.open('core_classes.txt') {|io| io.each_line {|line| CORE_CLASS << line.chomp}}
-  #DEFINED_COLLECTIONS = [CORE_CLASS] # planned to use, but unneeded for now
+  PARAM_TYPES       = [:on_const, :on_ident, :on_ivar, :on_cvar]
+  IGNORE_TYPES      = [:on_sp, :on_nl, :on_ignored_nl, :on_rparen, :on_lparen]
+  SPECIAL_CASES     = [:disjoint_code_seperator] #need to work on naming here
+  CORE_CLASSES_FILE = File.expand_path("../../data/core_classes.txt", __FILE__)
+  CORE_CLASS        = File.readlines(CORE_CLASSES_FILE).map &:chomp
 end
-
