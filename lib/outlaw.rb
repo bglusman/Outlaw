e42b944898ce7290a940d45d02a250182959c55a
bglusman <brian@enjyn.com>
2012-01-24 11:05:52 -0500

    basic working binary

--- lib/outlaw.rb
+++ lib/outlaw.rb
@@ -2,14 +2,15 @@ require 'ripper'
 require_relative 'outlaw/law_dsl'
 require_relative 'outlaw/enforcement'
 require_relative 'outlaw/rule'
-module Outlaw
-  def self.outlaw(restriction, message)
-    law = LawDSL.parse(restriction, message)
-    Enforcement.add(law)
-  end
 
+def outlaw(restriction, message)
+  law = Outlaw::LawDSL.parse(restriction, message)
+  Outlaw::Enforcement.add(law)
+end
+
+module Outlaw
   def self.enforce(dir=".")
-    Enforcement.process_directory(dir)
+    Outlaw::Enforcement.process_directory(dir)
   end
   PARAM_TYPES         = [:on_const, :on_ident, :on_ivar, :on_cvar]
   IGNORE_TYPES        = [:on_sp, :on_nl, :on_ignored_nl, :on_rparen, :on_lparen]
@@ -17,6 +18,5 @@ module Outlaw
   CORE_CLASS          = []
   File.open('core_classes.txt') {|io| io.each_line {|line| CORE_CLASS << line.chomp}}
   #DEFINED_COLLECTIONS = [CORE_CLASS] # planned to use, but unneeded for now
-
-
 end
+
